6042ab0482bb717b7062069d0d865f6282fe8fb2
Parthkumar49 <parthkumar1430@gmail.com>
2025-12-07 17:54:44 +0530

    Add files via upload

---
+++ Prime_Number.js
@@ -0,0 +1,24 @@
+let num = 4;   // change this to 1, 2, 4 or any number you want
+
+if (num === 1) {
+    console.log("Neither prime nor composite");
+} 
+else if (num === 2) {
+    console.log("Prime Number");
+} 
+else {
+    let isPrime = true;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log("Prime Number");
+    } else {
+        console.log("Composite Number");
+    }
+}
